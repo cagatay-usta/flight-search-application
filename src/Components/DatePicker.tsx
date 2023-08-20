c2b6811715a2cf92803436a33bc75a57de1fc3bb
cagatay-usta <cagatay_usta@windowslive.com>
2023-08-20 17:14:40 +0300

    update mobile datepicker dısplay

--- src/Components/DatePicker.tsx
+++ src/Components/DatePicker.tsx
@@ -1,5 +1,5 @@
 import { add, format } from "date-fns";
-import { useState } from "react";
+import { useRef, useState } from "react";
 import { DayPicker } from "react-day-picker";
 import "react-day-picker/dist/style.css";
 import { useSearchContext } from "../Contexts/SearchContext";
@@ -19,10 +19,11 @@ function DatePicker({ oneWay = false, label }: DateProps) {
   const today = new Date();
   const { dates, handleDates, validationAlert } = useSearchContext();
   const [open, setOpen] = useState(false);
+  const width = useRef(window.innerWidth);
 
   let border = "border-white";
   if (validationAlert.departBigger) border = "border-red-400";
-  if (oneWay) border = "border-slate-300"
+  if (oneWay) border = "border-slate-300";
 
   const handleDaySelect = (date: Date | undefined) => {
     handleDates(date, dateType);
@@ -38,7 +39,11 @@ function DatePicker({ oneWay = false, label }: DateProps) {
 
   return (
     <div className="input-container flex-col flex relative flex-grow flex-shrink min-w-[205px]">
-      <p className={`absolute left-[.7rem] ${oneWay && "text-slate-100"} text-slate-400`}>
+      <p
+        className={`absolute left-[.7rem] ${
+          oneWay && "text-slate-100"
+        } text-slate-400`}
+      >
         {label}
       </p>
       <p
@@ -50,12 +55,14 @@ function DatePicker({ oneWay = false, label }: DateProps) {
       {open && !oneWay && (
         <DayPicker
           mode="single"
-          numberOfMonths={2}
+          numberOfMonths={width.current > 720 ? 2 : 1}
           selected={dateType == "return" ? dates.return : dates.depart}
           onSelect={handleDaySelect}
           fromDate={dateType == "return" ? dates.depart : today}
           toDate={add(today, { years: 1 })}
-          className="absolute -left-48 top-16 bg-slate-500 text-white p-6 rounded-xl z-10 shadow-xl "
+          className={`absolute  ${
+            width.current < 720 ? "-left-12 top-12" : "-left-48 top-16"
+          } bg-slate-500 text-white p-6 rounded-xl z-10 shadow-xl`}
         />
       )}
     </div>

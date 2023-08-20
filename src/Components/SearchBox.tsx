7997d73681a45eda6b867a1894ee98ec86aac3e5
cagatay-usta <cagatay_usta@windowslive.com>
2023-08-20 12:20:17 +0300

    convert searchbox to form

--- src/Components/SearchBox.tsx
+++ src/Components/SearchBox.tsx
@@ -9,14 +9,14 @@ function SearchBox() {
   return (
     <>
       <div className="flex justify-center  bg-sky-900">
-        <div className="search-box-container flex flex-wrap justify-center gap-1  text-gray-600 p-16 items-center">
+        <form action="#" className="search-box-container flex flex-wrap justify-center gap-1  text-gray-600 p-16 items-center">
           <AutoComplete id="from" />
           <AutoComplete id="to" />
           <OneWayBox />
           <DatePicker label="Depart" />
           <DatePicker label="Return" oneWay={oneWay} />
           <SearchButton />
-        </div>
+        </form>
       </div>
     </>
   );
